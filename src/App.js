3378e6ac73079e42192f687e47785b6f4c286fd9
goyal146 <ankushgoyal32@gmail.com>
2019-10-17 11:02:05 +0530

    add search filter

--- src/App.js
+++ src/App.js
@@ -3,31 +3,46 @@ import "./App.css";
 import RecipeComp from "./component/RecipeComp";
 const API_Key = "3e45306e92385956afb06806206d4424";
 
-
 class App extends Component {
   state = {
-    recipe:[]
-  }
+    recipe: [],
+    search: ""
+  };
   fetchRecipes = () => {
     fetch(`https://www.food2fork.com/api/search?key=${API_Key}`)
       .then(response => response.json())
       .then(data =>
         this.setState({
-          recipe:data.recipes
+          recipe: data.recipes
         })
-      )
-  }
-  componentDidMount(){
+      );
+  };
+  componentDidMount() {
     this.fetchRecipes();
   }
+  updateData = e => {
+    this.setState({ search: e.target.value.substr(0, 20) });
+  };
   render() {
-    console.log(this.state);
+    let filteredData = this.state.recipe.filter(recipe => {
+      return recipe.title.toLowerCase().indexOf(this.state.search) !== -1;
+    });
     return (
       <div className="App">
         <header className="App-header">
           <h1>Recipes</h1>
         </header>
-        <RecipeComp recipes = {this.state.recipe}/>
+        <form>
+        <input
+            type="text"
+            value={this.state.search}
+            placeholder="Search Recipe"
+            name="search"
+            className ="recipe-search"
+            onChange={this.updateData}
+          ></input>
+        </form>
+        <RecipeComp recipes={filteredData} />
       </div>
     );
   }
